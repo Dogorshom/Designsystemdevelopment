e72868cf6ca8f4bd6992bd169d328c5aa45376fb
Abdelsalam Dogorshom <saloom.dogorshom@gmail.com>
2025-12-23 10:54:53 +0300

    v

--- src/screens/NewOrderNotificationScreen.tsx
+++ src/screens/NewOrderNotificationScreen.tsx
@@ -202,7 +202,7 @@ export function NewOrderNotificationScreen() {
           <div className="bg-neutral-100 rounded-2xl p-4 mb-4">
             <div className="flex items-start gap-4">
               <img
-                src="https://images.unsplash.com/photo-1690162396384-6741ab2f33bd?w=50&h=50&fit=crop"
+                src="https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Customer"
                 className="w-12 h-12 rounded-full object-cover"
               />
